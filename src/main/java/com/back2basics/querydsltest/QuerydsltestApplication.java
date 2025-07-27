4125429b5b27f1b03bb02fdcfc97355fea631547
swlee <swo98@ajou.ac.kr>
2025-07-27 23:41:30 +0900

    init: project init

---
+++ src/main/java/com/back2basics/querydsltest/QuerydsltestApplication.java
@@ -0,0 +1,13 @@
+package com.back2basics.querydsltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydsltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydsltestApplication.class, args);
+    }
+
+}
