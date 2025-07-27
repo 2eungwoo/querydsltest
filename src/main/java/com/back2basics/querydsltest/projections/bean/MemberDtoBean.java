2a1a339c3a9151562760ce09350833869b4e081d
swlee <swo98@ajou.ac.kr>
2025-07-27 23:58:51 +0900

    fix: MemberDtoBean에서 setter를 어노테이션으로 변경

--- src/main/java/com/back2basics/querydsltest/projections/bean/MemberDtoBean.java
+++ src/main/java/com/back2basics/querydsltest/projections/bean/MemberDtoBean.java
@@ -1,9 +1,9 @@
 package com.back2basics.querydsltest.projections.bean;
 
+import lombok.Setter;
+
+@Setter
 public class MemberDtoBean {
     private String username;
     private int age;
-
-    public void setUsername(String username) { this.username = username; }
-    public void setAge(int age) { this.age = age; }
 }
